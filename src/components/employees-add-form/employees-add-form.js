f3cabc90eb8008ae8944a169090fe6fab698c1dd
Sofia <itssonyamisyura@gmail.com>
2026-02-22 16:42:45 +0100

    made app layout

---
+++ src/components/employees-add-form/employees-add-form.js
@@ -0,0 +1,24 @@
+import './employees-add-form.css';
+
+const EmployeesAddForm = () => {
+    return (
+        <div className="app-add-form">
+        <h3>Add a New Employee</h3>
+        <form 
+            className="add-form d-flex">
+             <input type="text"
+                className="form-control new-post-label"
+                placeholder="Enter employee name" />
+            <input type="number"
+                className="form-control new-post-label"
+                placeholder="Enter salary in $" />
+
+            <button type="submit"
+                className="btn btn-outline-light">Добавить</button>
+
+        </form>
+     </div>
+    )
+}
+
+export default EmployeesAddForm;
