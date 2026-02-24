84049687ebbad2d99702f6eb73720498b018a663
Sofia <itssonyamisyura@gmail.com>
2026-02-24 18:30:05 +0100

    onToggleIncrease,  onToggleRise

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,12 +1,12 @@
 import './app-info.css';
 
 
-const AppInfo = () => {
+const AppInfo = ({increased, employees}) => {
     return (
         <div className="app-info">
             <h1>Employee Management System in Company N</h1>
-            <h2>Total number of employees:</h2>
-            <h2>Receiving a bonus:</h2>
+            <h2>Total number of employees: {employees}</h2>
+            <h2>Receiving a bonus: {increased}</h2>
         </div>
     )
 }
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,9 +13,9 @@ class App extends Component {
         super(props);
         this.state = {
             data: [ 
-                {name: 'John H.', salary: 800, increase: false, id:1},
-                {name: 'Alex M.', salary: 3000, increase: true, id:2},
-                {name: 'Carla W.', salary: 5000, increase: false, id:3},
+                {name: 'John H.', salary: 800, increase: false, rise:true, id:1},
+                {name: 'Alex M.', salary: 3000, increase: true, rise:false, id:2},
+                {name: 'Carla W.', salary: 5000, increase: false, rise:false, id:3},
             ]
         }
         this.maxId = 4;
@@ -34,6 +34,7 @@ class App extends Component {
             name,
             salary, 
             increase: false,
+            rise: false,
             id: this.maxId++
         }
         this.setState(({data}) => ({
@@ -41,10 +42,52 @@ class App extends Component {
         })) //spread создаёт новый массив
     }
 
+    onToggleIncrease = (id) => {
+        // this.setState(({data}) => {
+        //     const index = data.findIndex(elem => elem.id === id);
+
+        //     const old = data[index]; // старый объект
+        //     const newItem = {...old, increase: !old.increase};
+        //     const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)]
+
+        //     return {
+        //         data: newArr
+        //     }
+        // })
+
+        //1) return new obj со свойством data: формируем новый массив map
+        //2) когда идет перебор объектов, если совпадают item.id === id - нашли нужный объект, возвращаем новый объект, который содержит сущность {...item, increase: !item.increase} 
+        //3) условие не совпало - return obj
+        //4) получаем массив объектов с измененным значением 
+
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, increase: !item.increase} 
+                }
+                return item;
+            })
+        }))
+    }
+
+    onToggleRise = (id) => {
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, rise: !item.rise} 
+                }
+                return item;
+            })
+        }))
+    }
+
     render() {
+        const employees = this.state.data.length; 
+        const increased = this.state.data.filter(item => item.increase === true).length;
+     
         return (
             <div className="app">
-                <AppInfo/>
+                <AppInfo employees={employees} increased={increased} />
     
                 <div className="search-panel">
                 <SearchPanel/>
@@ -53,7 +96,9 @@ class App extends Component {
     
                 <EmployeesList 
                     data={this.state.data}
-                    onDelete={this.deleteItem}/>
+                    onDelete={this.deleteItem}
+                    onToggleIncrease={this.onToggleIncrease}
+                    onToggleRise={this.onToggleRise}/>
                 <EmployeesAddForm onAdd={this.addItem} />
             </div>
         );
